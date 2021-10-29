559930e93f340421a52d4cf3417c70b84fcc5132
mathuscada <matheusbtourinho@gmail.com>
2021-10-29 15:18:38 -0300

    Corrigindo a q2 que eu tinha confundido na interpretação.

--- q2.js
+++ q2.js
@@ -10,6 +10,6 @@ const vencerGabriel = (arthur) => vencerEduardo(arthur) && arthur.pet;
 const vencerFelipe = (arthur) => {
     return vencerEduardo(arthur) && vencerGabriel(arthur) && arthur.carteiraDeTrabalho;
 };
-const entrarNaSurdina = ({relogio}) => relogio===18;
+const entrarNaSurdina = ({relogio}) => relogio>=18;
 
 console.log((vencerFelipe(Arthur)||entrarNaSurdina(Arthur)) ? "Arthur conseguiu vencer a GP!" : "Arthur falhou em vencer a GP");
